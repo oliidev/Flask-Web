6848e55ae452cfe1b4f448b5e43004749c2986db
Olii <123281522+oliidev@users.noreply.github.com>
2025-06-10 15:23:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def hello():
+  page = """
+    <h1>Here's a random number: {0}</h1>
+    <form>
+      <button>New Number</button>
+    </form>
+  """
+
+  num = random.randint(1, 25)  # Select a random integer from 1 - 25.
+  return page.format(num)
+
+@app.route('/goodbye')
+def goodbye():
+  message = "<h2>This is the second page!</h2>"
+  return message
+
+@app.route('/form')
+def form():
+  return render_template("index.html")
+
+@app.route('/another')
+def anotherone():
+  return render_template("another.html")
+
+
+
+if __name__ == '__main__':
+  app.run()
