29d1a9128e2aca6bad1b0d423fea5639690dc628
Olii <123281522+oliidev@users.noreply.github.com>
2025-06-12 15:21:16 +0100

    update coffee

--- main.py
+++ main.py
@@ -4,31 +4,40 @@ import random
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello():
-  page = """
-    <h1>Here's a random number: {0}</h1>
-    <form>
-      <button>New Number</button>
-    </form>
-  """
+# @app.route('/')
+# def hello():
+#   page = """
+#     <h1>Here's a random number: {0}</h1>
+#     <form>
+#       <button>New Number</button>
+#     </form>
+#   """
+#
+#   num = random.randint(1, 25)  # Select a random integer from 1 - 25.
+#   return page.format(num)
+#
+# @app.route('/goodbye')
+# def goodbye():
+#   message = "<h2>This is the second page!</h2>"
+#   return message
+#
+# @app.route('/form')
+# def form():
+#   return render_template("index.html")
+#
+# @app.route('/another')
+# def anotherone():
+#   return render_template("another.html")
+#
+# @app.route('/thanks')
+# def thanks():
+#   person = "Bob"
+#   action = "dancing"
+#   return render_template("tynote.html", name=person, verb=action)
 
-  num = random.randint(1, 25)  # Select a random integer from 1 - 25.
-  return page.format(num)
-
-@app.route('/goodbye')
-def goodbye():
-  message = "<h2>This is the second page!</h2>"
-  return message
-
-@app.route('/form')
-def form():
-  return render_template("index.html")
-
-@app.route('/another')
-def anotherone():
-  return render_template("another.html")
-
+@app.route('/landingpage')
+def landingpage():
+  return render_template("landingpage.html")
 
 
 if __name__ == '__main__':
